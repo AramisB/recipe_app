50e3a60949c21dee1ec1cd8c37cb389ac5e6c765
AramisB <brendachem7@gmail.com>
2024-07-08 01:43:41 +0300

    recipes api

--- recipe-backend/server.js
+++ recipe-backend/server.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://seal-app-jx46s.ondigitalocean.app/recipes'],
+  origin: ['http://localhost:3000', 'https://seal-app-jx46s.ondigitalocean.app/api/recipes'],
   credentials: true
 }));
 
